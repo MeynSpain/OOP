5ff260a9e3e82a8e29bb26c1e62fa5e0eba75b88
Kibirev Mikhail <93863789+Mikibirev@users.noreply.github.com>
2021-11-27 18:47:22 +0900

    Delete oop_program_array directory

--- oop_program_array/head_array.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-namespace kib {
-	int* Filling(int* mas, int di, int l);   // Ввод массива
-	void Output(int* mas, int l);           // Вывод массива
-	int Sum(int* mas, int l);             // Сумма массива
-}
